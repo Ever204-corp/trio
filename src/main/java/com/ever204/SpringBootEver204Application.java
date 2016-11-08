1ae8419cebc918d1713e099df0558f9c1e84778d
LearnToYou <lbai@ebay.com>
2016-11-08 17:32:16 +0800

    new project

---
+++ src/main/java/com/ever204/SpringBootEver204Application.java
@@ -0,0 +1,12 @@
+package com.ever204;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEver204Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEver204Application.class, args);
+	}
+}
